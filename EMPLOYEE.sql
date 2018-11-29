4378875cfc4f30a428be9be265ba6e1032398893
Eve Lin <34218569+Eve-Lin@users.noreply.github.com>
2018-11-29 22:08:22 +0200

    Sample SQL table

---
+++ EMPLOYEE.sql
@@ -0,0 +1,7 @@
+cretae table EMPLOYEE (
+    id INT NOT NULL auto _increment,
+    first_name VARCHAR(20) default NULL,
+    last_name VARCHAR(20)  default NULL,
+    salary    INT default NULL,
+    PRIMARY KEY (id) 
+);
